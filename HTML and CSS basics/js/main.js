9061e73306d312c8ced7b8970a1b8b9d7320825f
OksankaLV <oksanka.volyk@gmail.com>
2023-11-14 14:17:13 +0200

    ready
    ready

---
+++ HTML and CSS basics/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    mouseDrag: true,
+    touchDrag: true,
+   // nav:true,
+      dots: true,
+      nav: true,
+    navigationText: ["<",">"]
+  });
+})
+
+$(function () {
+
+});
